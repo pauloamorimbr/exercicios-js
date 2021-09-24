3c4774bf35dae72c625e2058e3103cb0449896d7
Paulo Amorim <pauloamorimbr@gmail.com>
2021-09-24 16:53:50 -0300

    ex1

---
+++ ex1-reverse-string.js
@@ -0,0 +1,18 @@
+/*
+Considerando os conceitos e comandos básicos de algoritmos:
+- Variável (let, const... )
+- Condicional (if)
+- Loop (for, forEach, etc...)
+
+E os métodos do tipo de váriável String:
+- <str>.charAt( index ) => Retorna o caráctere no índice passado como parâmetro
+- <str>.length => Retorna a quantidade de caracteres (tamanho em bytes) da string
+
+Coloque em uma variável e imprima na tela/console o conteúdo invertido da
+variável `inputName`
+*/
+
+const inputName = "Algum texto que será revertido"
+
+// Boa sorte!
+// Seu código aqui em baixo...
